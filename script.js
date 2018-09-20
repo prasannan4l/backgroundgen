94dd187d687a79c21574a9e273de0c3a389e31e6
Prasanna Kumaresan <prasannahlnz@gmail.com>
2018-09-20 22:03:12 +1200

    background gen commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var h3 = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var dombody = document.querySelector("body");
+
+
+function setBodyBackground(){
+  clearTextValue();
+  dombody.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  setTextValue();
+}
+
+function setTextValue(){
+  h3.innerText = "Gradient Value : linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  h3.classList.add("animated", "tada");
+}
+
+function clearTextValue(){
+  h3.innerText = "";
+}
+
+color1.addEventListener("input", setBodyBackground);
+color2.addEventListener("input", setBodyBackground);
