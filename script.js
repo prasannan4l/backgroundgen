94472baa93493beb4dca8921020854411d488f22
Prasanna Kumaresan <prasannahlnz@gmail.com>
2018-09-23 16:31:00 +1200

    added bundle.js, package.json and .gitignore

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var lodash = require('lodash');
+
+var aray = [1,2,3,4,5,6,7,8];
+console.log('answer:', lodash.without(aray, 4));
+
 var h3 = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
